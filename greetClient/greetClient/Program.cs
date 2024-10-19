7429c7248147153ec2a5a0a346d896071b3143e6
jeremy woo <jeremy>
2024-10-19 17:34:41 +1300

    init repo greeter

---
+++ greetClient/greetClient/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("https://localhost:7294");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+Console.WriteLine(reply.Message);
+
+
